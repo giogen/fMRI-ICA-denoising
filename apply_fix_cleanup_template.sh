6d973538ca6aee04e57eb5225fe2aa26b0c8ef43
Giovanni Gentile <gentile.giovanni@gmail.com>
2016-11-29 16:24:47 -0800

    Update apply_fix_cleanup_template.sh

--- apply_fix_cleanup_template.sh
+++ apply_fix_cleanup_template.sh
@@ -18,7 +18,7 @@ subject=001
 run=1
 
 # Run classifier (classify components into signal and noise)
-${fix_dir}fix -c ${output_dir}${subject}/run${run}_melodic.ica ${code_dir}${classifier_name} ${threshold}
+${fix_dir}fix -c ${output_dir}${subject}/run${run}_melodic.ica ${classifier_dir}${classifier_name} ${threshold}
 
 # Apply cleanup (remove noise components)
 ${fix_dir}fix -a ${output_dir}${subject}/run${run}_melodic.ica/fix4melview_${classifier_name}_thr${threshold}.txt
